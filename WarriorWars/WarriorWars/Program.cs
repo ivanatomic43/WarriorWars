d583e2c85c0fa652c4575d3aeef63e356e9876d8
ivanatomic43 <ivana.tomic96@gmail.com>
2020-10-14 21:44:40 +0200

    Basics of OOP with C#...

---
+++ WarriorWars/WarriorWars/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using WarriorWars.Enum;
+
+namespace WarriorWars
+{
+    class Program
+    {
+        static Random rnd = new Random();
+        static void Main()
+        {
+            Warrior goodGuy = new Warrior("Bob", Faction.GoodGuy);
+            Warrior badGuy = new Warrior("Joe", Faction.BadGuy);
+
+            while (goodGuy.IsAlive && badGuy.IsAlive)
+            {
+                if (rnd.Next(0,10) < 5)
+                {
+                    goodGuy.Attack(badGuy);
+                }
+                else
+                {
+                    badGuy.Attack(goodGuy);
+                }
+
+                Thread.Sleep(300);
+            }
+        }
+    }
+}
